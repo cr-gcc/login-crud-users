1e1cc64024542ef725d9650e4fcd43ebcb543aaf
Cris GC <cris.gcc@outlook.com>
2025-05-27 12:19:50 -0600

    Rutas base

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,17 @@ Route::get('/dashboard', [UserController::class, 'index'])
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //  Users
+        Route::post('/user/add', [UserController::class, 'add'])->name('profile.add');
+        Route::post('/user', [UserController::class, 'store'])->name('profile.store');
+        Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('profile.edit');
+        Route::patch('/user', [UserController::class, 'update'])->name('profile.update');
+        Route::get('/user/{id}', [UserController::class, 'show'])->name('profile.show');
+        Route::delete('/user', [UserController::class, 'destroy'])->name('profile.destroy');
+    //  Profile
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
